2941f32832bfca6346240ce2723ae3eb09a00820
Dennis Ploetner <re@lloc.de>
2025-05-05 18:20:55 +0200

    Composer setup & class stub

---
+++ src/class-multisite-improvements.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Compatibility & Implementation for WordPress.
+ *
+ * @package Multisite_Improvements
+ */
+
+/**
+ * Class Multisite_Improvements
+ */
+class Multisite_Improvements {
+
+	/**
+	 * Hook into WordPress.
+	 *
+	 * @return void
+	 */
+	public static function init(): void {
+	}
+}
