5e8b6d2c6c4cfbbc17a97ce40775cba3946f2d4a
Dennis Ploetner <re@lloc.de>
2025-05-06 16:10:44 +0200

    Code style issues fixed

--- tests/phpunit/TestLoader.php
+++ tests/phpunit/TestLoader.php
@@ -1,11 +1,29 @@
 <?php
+/**
+ * Loader Tests
+ *
+ * @package multisite-improvements-tests
+ */
+
+declare( strict_types=1 );
 
 namespace Syde\MultisiteImprovementsTests;
 
 use Syde\MultisiteImprovements\Loader;
 
+/**
+ * Test the Loader class.
+ *
+ * @internal
+ * @coversDefaultClass \Syde\MultisiteImprovements\Loader
+ */
 class TestLoader extends UnitTestCase {
 
+	/**
+	 * Test the init method.
+	 *
+	 * @return void
+	 */
 	public function test_init(): void {
 		$this->expectNotToPerformAssertions();
 
