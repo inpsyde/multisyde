898e9aa00fcb691b538e4b93ddd28f0bfae1f542
Dennis Ploetner <re@lloc.de>
2025-05-07 19:03:27 +0200

    CI run should work

--- tests/php/integration/bootstrap.php
+++ tests/php/integration/bootstrap.php
@@ -5,15 +5,6 @@
  * @package multisite-improvements-integration-tests
  */
 
-// Force Multisite mode before loading WP.
-define( 'WP_ALLOW_MULTISITE', true );
-define( 'MULTISITE', true );
-define( 'SUBDOMAIN_INSTALL', false );
-define( 'DOMAIN_CURRENT_SITE', 'localhost' );
-define( 'PATH_CURRENT_SITE', '/' );
-define( 'SITE_ID_CURRENT_SITE', 1 );
-define( 'BLOG_ID_CURRENT_SITE', 1 );
-
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
@@ -31,5 +22,7 @@ function _manually_load_plugin() {
 	require dirname( __DIR__, 3 ) . '/index.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', dirname( __DIR__, 3 ) . '/vendor/yoast/phpunit-polyfills' );
 
 require $_tests_dir . '/includes/bootstrap.php';
