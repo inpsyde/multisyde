8dbb62cc9e385c691c2c27ed4c746fbd52fea27d
Dennis Ploetner <re@lloc.de>
2025-05-25 14:04:10 +0200

    Codestyle

--- tests/php/unit/TestPresenter.php
+++ tests/php/unit/TestPresenter.php
@@ -41,9 +41,9 @@ class TestPresenter extends UnitTestCase {
 	 */
 	public function test_add_network_admin_menu() {
 		$modules = \Mockery::mock( Modules::class );
-        $test = new Presenter( $modules );
+		$test    = new Presenter( $modules );
 
-        Functions\expect( 'add_menu_page' )->once()->with(
+		Functions\expect( 'add_menu_page' )->once()->with(
 			'Multisyde',
 			'Multisyde',
 			'manage_network',
@@ -62,32 +62,43 @@ class TestPresenter extends UnitTestCase {
 	 */
 	public function test_render_overview_page(): void {
 		$modules = \Mockery::mock( Modules::class );
-        $modules->shouldReceive( 'get_presentable_features' )->andReturn( array(
-            Modules\SiteActivePlugins\Feature::class => Modules\SiteActivePlugins\About::class,
-            Modules\GetSiteBy\Feature::class => Modules\GetSiteBy\About::class,
-        ) );
+		$modules->shouldReceive( 'get_presentable_features' )->andReturn(
+			array(
+				Modules\SiteActivePlugins\Feature::class => Modules\SiteActivePlugins\About::class,
+				Modules\GetSiteBy\Feature::class         => Modules\GetSiteBy\About::class,
+			)
+		);
 
 		( new Presenter( $modules ) )->render_overview_page();
 
 		$this->expectOutputRegex( '/.*Multisyde.*This plugin provides various improvements for WordPress multisite installations.*/' );
 	}
 
-    public function test_get_admin_footer_text(): void {
-        Functions\expect('get_current_screen')->andReturn((object) ['id' => 'toplevel_page_multisyde-network']);
+	/**
+	 * Test the get_admin_footer_text method.
+	 *
+	 * @return void
+	 */
+	public function test_get_admin_footer_text(): void {
+		Functions\expect( 'get_current_screen' )->andReturn( (object) array( 'id' => 'toplevel_page_multisyde-network' ) );
 
-        $result = Presenter::get_admin_footer_text('Original footer text');
-        $expected = 'Made with <span class="dashicons dashicons-heart"></span> by <a href="https://syde.com" target="_blank" rel="noopener noreferrer">Syde</a>.';
+		$result   = Presenter::get_admin_footer_text( 'Original footer text' );
+		$expected = 'Made with <span class="dashicons dashicons-heart"></span> by <a href="https://syde.com" target="_blank" rel="noopener noreferrer">Syde</a>.';
 
-        $this->assertEquals($expected, $result);
-    }
+		$this->assertEquals( $expected, $result );
+	}
 
-    public function test_get_admin_footer_text_elsewhere(): void {
-        Functions\expect('get_current_screen')->andReturn((object) ['id' => 'elsewhere_page']);
+	/**
+	 * Test the get_admin_footer_text method when not on the Multisyde network admin page.
+	 *
+	 * @return void
+	 */
+	public function test_get_admin_footer_text_elsewhere(): void {
+		Functions\expect( 'get_current_screen' )->andReturn( (object) array( 'id' => 'elsewhere_page' ) );
 
-        $text = 'Original footer text';
-        $result = Presenter::get_admin_footer_text($text);
+		$text   = 'Original footer text';
+		$result = Presenter::get_admin_footer_text( $text );
 
-        $this->assertEquals($text, $result);
-    }
-
+		$this->assertEquals( $text, $result );
+	}
 }
