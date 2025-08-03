f5f97e5e27146bc5098bf6124f9e3361d69f767a
Dennis Ploetner <re@lloc.de>
2025-08-03 18:49:12 +0200

    Doc strings for test methods

--- modules/SiteActiveTheme/tests/unit/TestFeature.php
+++ modules/SiteActiveTheme/tests/unit/TestFeature.php
@@ -34,12 +34,22 @@ final class TestFeature extends UnitTestCase {
 		Feature::init();
 	}
 
+	/**
+	 * Test the static add_column method.
+	 *
+	 * @return void
+	 */
 	public function test_add_column(): void {
 		$expected = array( Feature::COLUMN_NAME => 'Active Theme' );
 
 		$this->assertSame( $expected, Feature::add_column( array() ) );
 	}
 
+	/**
+	 * Test the static manage_custom_column method with any other column name incoming.
+	 *
+	 * @return void
+	 */
 	public function test_manage_custom_column_any_other_column(): void {
 		Functions\expect( 'switch_to_blog' )->never();
 		Functions\expect( 'restore_current_blog' )->never();
@@ -47,6 +57,11 @@ final class TestFeature extends UnitTestCase {
 		Feature::manage_custom_column( 'abitrary_column', 1 );
 	}
 
+	/**
+	 * Test the static manage_custom_column method with a theme without a parent theme.
+	 *
+	 * @return void
+	 */
 	public function test_manage_custom_column_theme_without_parent_theme(): void {
 		$theme = \Mockery::mock( WP_Theme::class );
 		$theme->shouldReceive( 'get' )->twice()->andReturnArg( 0 );
@@ -61,6 +76,11 @@ final class TestFeature extends UnitTestCase {
 		Feature::manage_custom_column( Feature::COLUMN_NAME, 1 );
 	}
 
+	/**
+	 * Test the static manage_custom_column method with a theme with a parent theme.
+	 *
+	 * @return void
+	 */
 	public function test_manage_custom_column_theme_with_parent_theme(): void {
 		$parent = \Mockery::mock( WP_Theme::class );
 		$parent->shouldReceive( 'get' )->once()->andReturn( 'Parent Theme' );
